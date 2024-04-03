6033e1885e42e2c2b32765e4aacf74e721b4d629
Sophie LAURENT <sophielaurent92@gmail.com>
2024-04-03 09:23:40 +0200

    register feature ok in front end

---
+++ front/src/app/features/auth/components/register/register.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegisterRequest } from '../../interfaces/registerRequest.interface';
+import { AuthService } from '../../services/auth-service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+
+  public onError = false;
+
+  private passwordRegx: RegExp = /^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=.*[@$!%*#?&^_-])(?=\D*\d).{8,}$/;
+
+  constructor(private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router) {
+  }
+
+  public form = this.formBuilder.group({
+
+    username: [
+      '',
+      [
+        Validators.required,
+        Validators.min(6),
+        Validators.max(20)
+      ]
+    ]
+    ,
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.min(8),
+        Validators.pattern(this.passwordRegx)
+      ]
+    ]
+  })
+
+
+  public submit(): void {
+    const registerRequest = this.form.value as RegisterRequest;
+    this.authService.register(registerRequest).subscribe({
+      next: (_: void) => this.router.navigate(['/login']),
+      error: _ => this.onError = true,
+    }
+    );
+  }
+
+}
