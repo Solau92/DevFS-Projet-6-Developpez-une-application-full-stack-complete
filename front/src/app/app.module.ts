84ef5b2d9ff544f73d91ed868cd49dc7477b26a5
Sophie LAURENT <sophielaurent92@gmail.com>
2024-04-19 13:27:15 +0200

    landing page design

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 const materialModule = [
   MatButtonModule,
@@ -36,6 +37,7 @@ const materialModule = [
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     ...materialModule
   ],
   providers: [ 
